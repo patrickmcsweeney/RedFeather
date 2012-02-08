8a513d541e05a0141aa0e0411d21df8ceb445e3c
Patrick McSweeney <mcsweeney.patrick@gmail.com>
2012-02-08 15:08:23 +0000

    Created basic framework
    Added facility to have pages.
    A page has a call list associated with it.

--- redfeather.php
+++ redfeather.php
@@ -1,11 +1,30 @@
 <?php
+error_reporting(E_ALL);
+$pages = array();
 $functions = array();
+$variables = array('page'=>'');
 
-function call($function_name, $arg_array)
+array_push($pages, 'resource');
+
+call_back_list('resource', array('render_top','render_resource','render_bottom'));
+
+if(isset($_REQUEST['page']))
 {
-	foreach( $function as $functions[$function_name] )
+	call($_REQUEST['page']);
+}
+else
+{
+	call('resource');
+}
+
+print $variables['page'];
+
+function call($function_name, $arg_array=array())
+{
+	global $functions;
+	foreach( $functions[$function_name] as $function )
 	{
-		call_user_func($function, $arg_array);
+		array_push($results, call_user_func($function));
 	}
 }
 
@@ -21,6 +40,30 @@ function call_back_list($function_name, $list=Null)
 		return array();
 	}
 
-	$functions[$function_name] = array();
+	$functions[$function_name] = $list;
 	return True;
+}
+
+function render_top()
+{
+	global $variables;
+	$variables['page'] .= 
+'<html><head>
+	<title>'.$variables['page_title'].'</title>
+</head><body>';
+}
+
+function render_resource()
+{
+	global $variables;
+	$variables['page'] .= 
+'<h1>RedFeather</h1>';
+}
+
+function render_bottom()
+{
+	global $variables;
+	$variables['page'] .=
+'</body>
+</html>';
 }
