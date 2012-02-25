e962c9565b923bcec4c7c0fc5d57de9fbacad5ad
Patrick McSweeney <mcsweeney.patrick@gmail.com>
2012-02-25 11:27:34 +0000

    * Half way though adding save data

--- redfeather.php
+++ redfeather.php
@@ -5,8 +5,10 @@ $functions = array();
 $variables = array('page'=>'');
 
 array_push($pages, 'resource');
+call_back_list('resource', array( 'load_data', 'render_top','render_resource','render_bottom'));
 
-call_back_list('resource', array('render_top','render_resource','render_bottom'));
+array_push($pages, 'manage_resources');
+call_back_list('manage_resources', array( 'load_data', 'render_top','render_manage_list','render_bottom'));
 
 if(isset($_REQUEST['page']))
 {
@@ -44,6 +46,13 @@ function call_back_list($function_name, $list=Null)
 	return True;
 }
 
+function load_data()
+{
+	global $variables;
+	
+	$variables['data'] = unserialize(file_get_contents("rf_meta.php"));
+}
+
 function render_top()
 {
 	global $variables;
@@ -56,8 +65,21 @@ function render_top()
 function render_resource()
 {
 	global $variables;
-	$variables['page'] .= 
-'<h1>RedFeather</h1>';
+	$variables['page'] .= '<h1>RedFeather</h1>';
+
+	foreach($variables['data'] as $file){
+		$variables['page'] .= "<div>".$variables['data'][$file]." - <a href='$file'>$file</a></div>";
+	}
+}
+
+function render_manage_list()
+{
+	global $variables;
+	$variables['page'] .= '<h1>RedFeather</h1>';
+
+	foreach($variables['data'] as $file){
+		$variables['page'] .= "<div>".$variables['data'][$file]." - <a href='$file'>$file</a></div>";
+	}
 }
 
 function render_bottom()
