a12e82c2f48fa57527b9538ae551710b51a1d70b
Patrick McSweeney <mcsweeney.patrick@gmail.com>
2012-03-16 17:21:20 +0000

    *Saving and loading works! still a few warnings...

--- redfeather.php
+++ redfeather.php
@@ -1,4 +1,6 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('log_errors', 1); 
 error_reporting(E_ALL);
 $pages = array();
 $functions = array();
@@ -8,7 +10,7 @@ array_push($pages, 'resource');
 call_back_list('resource', array( 'load_data', 'render_top','render_resource','render_bottom'));
 
 array_push($pages, 'manage_resources');
-call_back_list('manage_resources', array( 'load_data', 'render_top','render_manage_list','render_bottom'));
+call_back_list('manage_resources', array( 'load_data', 'save_data', 'load_data', 'render_top','render_manage_list','render_bottom'));
 
 if(isset($_REQUEST['page']))
 {
@@ -26,7 +28,7 @@ function call($function_name, $arg_array=array())
 	global $functions;
 	foreach( $functions[$function_name] as $function )
 	{
-		array_push($results, call_user_func($function));
+		call_user_func($function);
 	}
 }
 
@@ -39,6 +41,7 @@ function call_back_list($function_name, $list=Null)
 		{
 			return $functions[$function_name];
 		}
+		
 		return array();
 	}
 
@@ -51,6 +54,26 @@ function load_data()
 	global $variables;
 	
 	$variables['data'] = unserialize(file_get_contents("rf_meta.php"));
+	if(! is_array($variables["data"]) )
+	{
+		$variables['data'] = array();
+	}
+}
+
+function save_data()
+{
+	if(is_array($_REQUEST["filenames"]))
+	{	
+		for($i=0; $i < count($_REQUEST["filenames"]); $i++)
+		{
+			$variables["data"][$_REQUEST["filenames"][$i]]["title"] = $_REQUEST["titles"][$i];
+			$variables["data"][$_REQUEST["filenames"][$i]]["description"] = $_REQUEST["descriptions"][$i];
+		}
+		$fh = fopen("rf_meta.php", "w");
+		fwrite($fh,serialize($variables['data']));
+		fclose($fh);
+
+	}
 }
 
 function render_top()
@@ -77,15 +100,48 @@ function render_manage_list()
 	global $variables;
 	$variables['page'] .= '<h1>RedFeather</h1>';
 
-	foreach($variables['data'] as $file){
-		$variables['page'] .= "<div>".$variables['data'][$file]." - <a href='$file'>$file</a></div>";
+	$manage_list = "";
+	$dir = "./";
+	if ($dh = opendir($dir)) {
+
+		#Probably breaks with windows and other things which dont use /
+		$php_file = array_pop(explode("/", $_SERVER["SCRIPT_NAME"]));
+		$variables["page"] .= "<form action='$php_file?page=manage_resources' method='POST'>\n";
+		while (($file = readdir($dh)) !== false) {
+			
+			if(is_dir($dir.$file)){continue;}
+			if($file == $php_file){continue;}
+			if($file == "rf_meta.php"){continue;}
+
+			$file_line =  "filename: $file : filetype: " . filetype($dir . $file) . "<br />\n";
+			$data = $variables["data"]["$file"];
+			$variables["page"] .= sprintf( <<<BLOCK
+<div class="metadata-input">
+<input name="titles[]" value="%s" /> <br />
+<textarea name="descriptions[]">%s</textarea> <br />
+<select name="licence">
+	<option value="foo bar baz" />
+</select><br />
+<input type="hidden" name="filenames[]" value="$file" />
+</div>
+BLOCK
+, $data["title"], $data["description"] );
+			
+		}
+		closedir($dh);
+		$variables["page"] .= "<input type='submit' value='Save' />\n";
+		$variables["page"] .= "</form>\n";
 	}
+
+	foreach($variables['data'] as $file => $val){
+		$variables['page'] .= "<div>".$variables['data'][$file]['title']." - <a href='$file'>$file</a></div>";
+	}
+	$variables["page"] .= $manage_list;
 }
 
 function render_bottom()
 {
 	global $variables;
-	$variables['page'] .=
-'</body>
+	$variables['page'] .= '</body>
 </html>';
 }
